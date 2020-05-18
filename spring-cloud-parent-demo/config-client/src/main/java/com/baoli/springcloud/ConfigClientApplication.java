39f2279616fcfd3a88f6e05359dc93678785cb34
libaojian2019 <libaodubbo@163.com>
2020-05-18 08:59:51 +0800

    bus 总线

--- spring-cloud-parent-demo/config-client/src/main/java/com/baoli/springcloud/ConfigClientApplication.java
+++ spring-cloud-parent-demo/config-client/src/main/java/com/baoli/springcloud/ConfigClientApplication.java
@@ -3,6 +3,9 @@ package com.baoli.springcloud;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
  * @create: 2020-05-14 16:03
  */
 @SpringBootApplication
+@EnableEurekaClient
+@EnableDiscoveryClient
 @RestController
+@RefreshScope
 public class ConfigClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigClientApplication.class, args);
